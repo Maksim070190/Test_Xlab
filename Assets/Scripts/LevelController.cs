7827ed5d62d87a10160120e0af369dbe165087d9
Maksim Khilko <m.khilko@carx-tech.com>
2025-01-30 17:28:34 +0300

    Complete_4_Unit_2

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Golf
+{
+    public class LevelController : MonoBehaviour
+    {
+        public SpawnerStone spawner;
+        public float delayMax = 2f;
+        public float delayMin = 0.5f;
+        public float delayStep = 0.1f;
+
+        private float m_delay = 0.5f;
+
+        private float m_lastSpawnedTime = 0;
+        private void Start()
+        {
+            m_lastSpawnedTime = Time.time;
+            RefreshDelay();
+        }
+
+        private void OnEnable()
+        {
+            Stone.onCollisionStone += GameOver;
+        }
+
+        private void OnDisable()
+        {
+            Stone.onCollisionStone -= GameOver;
+        }
+
+        private void GameOver()
+        {
+            Debug.Log("Game OVER!!!");
+            enabled = true;
+        }
+
+        public void RefreshDelay()
+        {
+            m_delay = UnityEngine.Random.Range(delayMin, delayMax);
+            delayMax = Mathf.Max(delayMin, delayMax - delayStep);
+        }
+
+        private void Update()
+        {
+                if(Time.time >= m_lastSpawnedTime + m_delay)
+                {
+                    spawner.Spawn();
+                    m_lastSpawnedTime = Time.time;
+
+                RefreshDelay();
+                }
+            
+        }
+
+
+
+    }
+}
